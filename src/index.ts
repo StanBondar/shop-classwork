927d1845e44a4833840d18d0f08b207896979568
Anton N <anton.nikolenko@medentee.com>
2021-11-11 19:20:58 +0200

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.get('/', async (req: Request, res: Response) => {
   console.log(req.url);
 
-  res.send('Im alive!');
+  res.send(`Im alive! ${port}`);
 });
 
 registerRouters(app);
